70e52c91bb7105594f4228b9be45bbabfe9449ea
Isra Mejia <ihmaisrael@gmail.com>
2024-09-10 22:44:34 -0600

    librerias externas bien

--- ConfigInicial/ConfigInicial/Modelado_Jerarquico.cpp
+++ ConfigInicial/ConfigInicial/Modelado_Jerarquico.cpp
@@ -8,7 +8,7 @@ Fecha de entrega: 08-09-24
 
 #include<iostream>
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h> 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
